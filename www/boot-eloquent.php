b5899d9483af36f8c21b90d7b0b72ce43471f2ea
Fabien <fabienbreuil5@gmail.com>
2016-03-18 18:37:06 +0100

    boot-eloquent

--- www/boot-eloquent.php
+++ www/boot-eloquent.php
@@ -2,14 +2,16 @@
 require "vendor/autoload.php";
 use Illuminate\Database\Capsule\Manager as DB;
 
+$tabConfig = parse_ini_file('config.ini');
+
 $capsule = new DB;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'hackathon',
-    'username'  => 'root',
-    'password'  => '',
+    'host'      => $tabConfig['host'],
+    'database'  => $tabConfig['database'],
+    'username'  => $tabConfig['username'],
+    'password'  => $tabConfig['password'],
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
@@ -17,4 +19,5 @@ $capsule->addConnection([
 
 
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+// $capsule->setAsGlobal();
 $capsule->bootEloquent();
