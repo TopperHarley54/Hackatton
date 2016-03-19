c66db26dcb7a29d980c550c6d6caaf12f5ceb7ac
Matteus54 <rapenne@.matthieu@telecomnancy.net>
2016-03-19 02:36:21 +0100

    Create controlCarte.js to open popup, and header.twig to open script

---
+++ www/app/js/controlCarte.js
@@ -0,0 +1,19 @@
+'use strict';
+$(function () {
+  window.controlCarte = {};
+
+  mymap.on('click', onMapClick);
+
+
+
+
+});
+
+
+
+function onMapClick(e) {
+
+    var popup = L.popup({maxWidth:50,maxHeight:50}).setLatLng(e.latlng).setContent("<div onclick=window.location.href='http://localhost/Hackatton/www/'> create new alert </div>").openOn(mymap);
+
+ 
+}
