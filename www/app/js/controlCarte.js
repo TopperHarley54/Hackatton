80eb8a31dd743cc30c096d625e4f5280d3b5a921
Fabien <fabienbreuil5@gmail.com>
2016-03-19 11:00:48 +0100

    carte js

--- www/app/js/controlCarte.js
+++ www/app/js/controlCarte.js
@@ -3,17 +3,41 @@ $(function () {
   window.controlCarte = {};
 
   mymap.on('click', onMapClick);
-
+  // var coo;
 
 
 
 });
 
-
+var coo;
 
 function onMapClick(e) {
 
-    var popup = L.popup({maxWidth:50,maxHeight:50}).setLatLng(e.latlng).setContent("<div onclick=window.location.href='alertForm'> create new alert </div>").openOn(mymap);
+// var affipopup=' <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">    Launch demo modal  </button>   <!-- Modal --> <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">  <div class="modal-dialog" role="document">    <div class="modal-content">      <div class="modal-header">        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>        <h4 class="modal-title" id="myModalLabel">Modal title</h4>      </div>      <div class="modal-body">        test      </div>      <div class="modal-footer">        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>        <button type="button" class="btn btn-primary">Save changes</button>      </div>    </div>  </div></div>';
 
- 
+
+    // var popup = L.popup({maxWidth:50,maxHeight:50}).setLatLng(e.latlng).setContent("<div onclick=window.location.href='alertForm'> create new alert </div>").openOn(mymap);
+    var popup = L.popup({maxWidth:50,maxHeight:50}).setLatLng(e.latlng).setContent("<div onclick=afficherPopUpEnregistrement()> create new alert </div>").openOn(mymap);
+    // var popup = L.popup({maxWidth:50,maxHeight:50}).setLatLng(e.latlng).setContent(affipopup).openOn(mymap);
+    coo=e.latlng;
+
+}
+
+function afficherPopUpEnregistrement(latlng){
+  // console.log('TEST');
+  // console.log(coo);
+  //coordonnées
+  //type de l'information dans un selecteur
+  //commentaire libre
+  var popupModif = $('<div>').attr('class','popup modal fade in');
+  var affCoordonnees = $('<div>').attr('class','coordonnees').val(latlng);
+  var affSelecteur = $('<div>').attr('class','selecteur');
+  var affCommentaire = $('<div>').attr('class','champCommentaire');
+  var boutonValidation = $('<button>').attr('class','boutonValider');
+
+popupModif.append(affCoordonnees);
+popupModif.append(affSelecteur);
+popupModif.append(affCommentaire);
+$($(popupModif)).modal('show');
+  // console.log(popupModif);
 }
