2ffc24aa84064d2e60d8d5d2caf6195453152b26
Matteus54 <rapenne@.matthieu@telecomnancy.net>
2016-03-19 12:29:07 +0100

    Add fonction alerte()

--- www/app/js/alerte.js
+++ www/app/js/alerte.js
@@ -1,21 +1,39 @@
-$(document).ready(function () {
-    if (typeof variable !== 'undefined'){
-    function alerte() {
-        $.ajax({
-          type: "GET",
-          url:"api/alerte",
-          contentType: "application/json" ,
-          success: function(data){
-            $($.parseJSON(data)).each(function (index, iteration) {
-                L.marker([iteration.lat, iteration.lng]).addTo(mymap)
-                .bindPopup('<b>'+iteration.type+'</b><br>'+iteration.commentaire);
-            });
-          },
-          error: function(){
-            console.log("fail...");
+
+var markerArray = new Array();
+
+
+function alerte() {
+
+
+        if(markerArray.length >= 1){
+          for(var i = 0; i < markerArray.length; i++){
+               mymap.removeLayer(markerArray[i]);
           }
-        });
+          markerArray = new Array();
+        }
+        else {
+
+          $.ajax({
+            type: "GET",
+            url:"api/alerte",
+            contentType: "application/json" ,
+            success: function(data){
+              console.log(data);
+              $($.parseJSON(data)).each(function (index, iteration) {
+
+                  var marker = L.marker([iteration.lat, iteration.lng]);
+                  mymap.addLayer(marker);
+                  marker.bindPopup('<b>'+iteration.type+'</b><br>'+iteration.commentaire);
+                  markerArray.push(marker);
+              });
+            },
+            error: function(){
+              console.log("fail...");
+            }
+          });
+
+      }
     }
-    alerte();
-    }
-});
+
+
+
