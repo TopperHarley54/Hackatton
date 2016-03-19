03176b1ba9edd1443329584b357772438f149200
Fabien <fabienbreuil5@gmail.com>
2016-03-19 03:47:32 +0100

    alertForm

--- www/app/routes.php
+++ www/app/routes.php
@@ -13,17 +13,16 @@ $app->get('/accueil', function () use ($app) {
     $c->accueil();
 })->name('accueil');
 
+$app->get('/alertForm', function () use ($app) {
+    $c = new Controller($app);
+    $c->alertform();
+})->name('api-alerte');
 
 $app->group('/api', function () use ($app) {
 
     $app->get('/alerte', function () use ($app) {
         $c = new ControllerAPI($app);
         $c->alerteAll();
-    })->name('api-alerte');
-
-    $app->get('/alerteForm', function () use ($app) {
-        $c = new Controller($app);
-        $c->alertform();
     })->name('api-alerte');
 
     $app->get('/alerte/:id', function ($id) use ($app) {
