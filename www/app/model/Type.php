b019a16fee0ecbd573392c1a8de5f201183d433b
Christophe <christophe.savoyen@gmail.com>
2016-03-19 09:43:33 +0100

    conflit

--- www/app/model/Type.php
+++ www/app/model/Type.php
@@ -5,3 +5,24 @@
  * Date: 19/03/2016
  * Time: 09:28
  */
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model{
+
+    protected $primaryKey = "id_type";
+    protected $table = "type";
+    public $timestamps = false;
+
+    public function alerte()
+    {
+        return $this->belongsTo('alerte', 'id_alerte');
+    }
+
+
+    public static function getAllDocuments($var)
+    {
+        array_push($var, 'type');
+        return Type::get($var);
+    }
+}
